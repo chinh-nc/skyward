42725117f6e7d1e3a553a5d00608f2439d5103f9
chinh-nc <congchinh705@gmail.com>
2023-10-08 00:01:46 +0700

    fix responsive mb

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
     type: "loop",
     perPage: 4,
     focus: 0,
+    breakpoints: {
+      1062: {
+        perPage: 3,
+      },
+      768: {
+        perPage: 2,
+      },
+      567: {
+        perPage: 1,
+      },
+    },
   }).mount();
   new Splide("#products", {
     type: "loop",
@@ -15,5 +26,13 @@ document.addEventListener("DOMContentLoaded", function () {
     focus: 0,
     updateOnMove: true,
     direction: "rtl",
+    breakpoints: {
+      1062: {
+        perPage: 2,
+      },
+      767: {
+        perPage: 3,
+      },
+    },
   }).mount();
 });
